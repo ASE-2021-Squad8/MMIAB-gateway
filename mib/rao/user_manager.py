17ba651c2c3d89cfe315631bfa399ec5a9d40ec3
Federico <vgfede@gmail.com>
2021-12-03 16:03:13 +0100

    create_user, change_pass, content_filter

--- mib/rao/user_manager.py
+++ mib/rao/user_manager.py
@@ -1,13 +1,13 @@
 from mib.auth.user import User
 from mib import app
-from flask_login import (logout_user)
+from flask_login import logout_user
 from flask import abort
 import requests
 
 
 class UserManager:
-    USERS_ENDPOINT = app.config['USERS_MS_URL']
-    REQUESTS_TIMEOUT_SECONDS = app.config['REQUESTS_TIMEOUT_SECONDS']
+    USERS_ENDPOINT = app.config["USERS_MS_URL"]
+    REQUESTS_TIMEOUT_SECONDS = app.config["REQUESTS_TIMEOUT_SECONDS"]
 
     @classmethod
     def get_user_by_id(cls, user_id: int) -> User:
@@ -18,81 +18,72 @@ class UserManager:
         :return: User obj with id=user_id
         """
         try:
-            response = requests.get(cls.USERS_ENDPOINT + "/user/" + str(user_id) + "/public", timeout=cls.REQUESTS_TIMEOUT_SECONDS)                        
+            response = requests.get(
+                cls.USERS_ENDPOINT + "/user/" + str(user_id) + "/public",
+                timeout=cls.REQUESTS_TIMEOUT_SECONDS,
+            )
             json_payload = response.json()
             if response.status_code == 200:
-                #user is authenticated
+                # user is authenticated
                 user = response
             else:
-                raise RuntimeError('Server has sent an unrecognized status code %s' % response.status_code)
+                raise RuntimeError(
+                    "Server has sent an unrecognized status code %s"
+                    % response.status_code
+                )
 
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
             return abort(500)
 
         return user
 
-    @classmethod
-    def get_user_by_email(cls, user_email: str):
-        """
-        This method contacts the users microservice
-        and retrieves the user object by user email.
-        :param user_email: the user email
-        :return: User obj with email=user_email
-        """
-        try:
-            response = requests.get(cls.USERS_ENDPOINT + 
-                                    timeout=cls.REQUESTS_TIMEOUT_SECONDS)
-            json_payload = response.json()
-            user = None
+    # @classmethod
+    # def get_user_by_email(cls, user_email: str):
+    #     """
+    #     This method contacts the users microservice
+    #     and retrieves the user object by user email.
+    #     :param user_email: the user email
+    #     :return: User obj with email=user_email
+    #     """
+    #     try:
+    #         response = requests.get(
+    #             cls.USERS_ENDPOINT + "/user_email/" + user_email,
+    #             timeout=cls.REQUESTS_TIMEOUT_SECONDS,
+    #         )
+    #         json_payload = response.json()
+    #         user = None
 
-            if response.status_code == 200:
-                user = User.build_from_json(json_payload)
+    #         if response.status_code == 200:
+    #             user = User.build_from_json(json_payload)
+    #         else response.status_code == 404:
+    #             return {"message": }
 
-        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            return abort(500)
+    #     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+    #         return abort(500)
 
-        return user
-
-    @classmethod
-    def get_user_by_phone(cls, user_phone: str) -> User:
-        """
-        This method contacts the users microservice
-        and retrieves the user object by user phone.
-        :param user_phone: the user phone
-        :return: User obj with phone=user_phone
-        """
-        try:
-            response = requests.get("%s/user_phone/%s" % (cls.USERS_ENDPOINT, user_phone),
-                                    timeout=cls.REQUESTS_TIMEOUT_SECONDS)
-            json_payload = response.json()
-            user = None
-
-            if response.status_code == 200:
-                user = User.build_from_json(json_payload)
-
-        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            return abort(500)
-
-        return user
+    #     return user
 
     @classmethod
-    def create_user(cls,
-                    email: str, password: str,
-                    firstname: str, lastname: str,
-                    birthdate, phone: str):
+    def create_user(
+        cls,
+        email: str,
+        password: str,
+        firstname: str,
+        lastname: str,
+        dateofbirth,
+    ):
         try:
-            url = "%s/user" % cls.USERS_ENDPOINT
-            response = requests.post(url,
-                                     json={
-                                         'email': email,
-                                         'password': password,
-                                         'firstname': firstname,
-                                         'lastname': lastname,
-                                         'birthdate': birthdate,
-                                         'phone': phone
-                                     },
-                                     timeout=cls.REQUESTS_TIMEOUT_SECONDS
-                                     )
+            response = requests.post(
+                cls.USERS_ENDPOINT + "/user",
+                json={
+                    "email": email,
+                    "password": password,
+                    "firstname": firstname,
+                    "lastname": lastname,
+                    "dateofbirth": dateofbirth,
+                },
+                timeout=cls.REQUESTS_TIMEOUT_SECONDS,
+            )
 
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
             return abort(500)
@@ -100,35 +91,28 @@ class UserManager:
         return response
 
     @classmethod
-    def update_user(cls, user_id: int, email: str, password: str, phone: str):
+    def update_user(cls, user_id: int, email: str, password: str):
         """
         This method contacts the users microservice
         to allow the users to update their profiles
-        :param phone:
         :param password:
         :param email:
         :param user_id: the customer id
             email: the user email
             password: the user password
-            phone: the user phone
         :return: User updated
         """
         try:
-            url = "%s/user/%s" % (cls.USERS_ENDPOINT, str(user_id))
-            response = requests.put(url,
-                                    json={
-                                        'email': email,
-                                        'password': password,
-                                        'phone': phone
-                                    },
-                                    timeout=cls.REQUESTS_TIMEOUT_SECONDS
-                                    )
+            url = cls.USERS_ENDPOINT + "/user/" + str(user_id)
+            response = requests.put(
+                url,
+                json={"email": email, "password": password},
+                timeout=cls.REQUESTS_TIMEOUT_SECONDS,
+            )
             return response
 
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
             return abort(500)
-
-        raise RuntimeError('Error with searching for the user %s' % user_id)
 
     @classmethod
     def delete_user(cls, user_id: int):
@@ -140,7 +124,7 @@ class UserManager:
         """
         try:
             logout_user()
-            url = "%s/user/%s" % (cls.USERS_ENDPOINT, str(user_id))
+            url = cls.USERS_ENDPOINT + "/user/" + str(user_id)
             response = requests.delete(url, timeout=cls.REQUESTS_TIMEOUT_SECONDS)
 
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
@@ -158,12 +142,13 @@ class UserManager:
         """
         payload = dict(email=email, password=password)
         try:
-            print('trying response....')
-            response = requests.post('%s/authenticate' % cls.USERS_ENDPOINT,
-                                     json=payload,
-                                     timeout=cls.REQUESTS_TIMEOUT_SECONDS
-                                     )
-            print('received response....')
+            print("trying response....")
+            response = requests.post(
+                cls.USERS_ENDPOINT + "/authenticate",
+                json=payload,
+                timeout=cls.REQUESTS_TIMEOUT_SECONDS,
+            )
+            print("received response....")
             json_response = response.json()
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
             # We can't connect to Users MS
@@ -173,10 +158,48 @@ class UserManager:
             # user is not authenticated
             return None
         elif response.status_code == 200:
-            user = User.build_from_json(json_response['user'])
+            user = User.build_from_json(json_response["user"])
             return user
         else:
             raise RuntimeError(
-                'Microservice users returned an invalid status code %s, and message %s'
-                % (response.status_code, json_response['error_message'])
+                "Microservice users returned an invalid status code %s, and message %s"
+                % (response.status_code, json_response["error_message"])
             )
+       
+    @classmethod
+    def change_password(cls, user_id: int, currpw: str, newpw: str, confpw: str):
+        """Call users microservice to change the password for a user
+        :param user_id: id of the user who wants to change the password
+        :param currpw: current password of the user who wants to change their password
+        :param newpw: new password chosen by the user
+        :param confpw: confirmation password, used to verify that the user has entered the currpw correctly
+        """
+        try:
+            url = cls.USERS_ENDPOINT + "/user/password/" + str(user_id),
+            response = requests.put(
+                url,
+                json={"currentpassword": currpw, "newpassword": newpw, "confirmpassword": confpw},
+                timeout=cls.REQUESTS_TIMEOUT_SECONDS,
+            )
+            return response
+
+        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+            return abort(500)
+
+    @classmethod
+    def set_content_filter(cls, id: int, filter: int):
+        """Call users microservice to change the content filter for a user
+
+        :param id: the user id
+        :param filter: 0 = disable, 1 = enable
+        :return: response
+        """
+        try:
+            response = requests.put(
+                cls.USERS_ENDPOINT + "/user/content_filter/" + str(id),
+                json={"filter": filter},
+                timeout=cls.REQUESTS_TIMEOUT_SECONDS
+            )
+            return response
+        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+            return abort(500)
--- mib/views/users.py
+++ mib/views/users.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, redirect, render_template, url_for, flash, request
-from flask_login import (login_user, login_required)
+import datetime
+
+from flask import Blueprint, redirect, render_template, url_for, request
+from flask_login import login_user, login_required, current_user
 
 from mib.forms import UserForm
 from mib.forms.forms import BlackListForm, ChangePassForm
 from mib.rao.user_manager import UserManager
 from mib.auth.user import User
+from flask import abort
+users = Blueprint("users", __name__)
 
-users = Blueprint('users', __name__)
 
-
-@users.route('/create_user/', methods=['GET', 'POST'])
+@users.route("/create_user", methods=["GET", "POST"])
 def create_user():
     """This method allows the creation of a new user into the database
 
@@ -17,178 +19,146 @@ def create_user():
         Redirects the user into his profile page, once he's logged in
     """
     form = UserForm()
-
-    if form.is_submitted():
-        email = form.data['email']
-        password = form.data['password']
-        firstname = form.data['firstname']
-        lastname = form.data['lastname']
-        birthdate = form.data['dateofbirth']
-        date = birthdate.strftime('%Y-%m-%d')
-        response = UserManager.create_user(
-            email,
-            password,
-            firstname,
-            lastname,
-            date
-        )
-        if response.status_code == 201:
-            # in this case the request is ok!
-            user = response.json()
-            to_login = User.build_from_json(user["user"])
-            login_user(to_login)
-            return redirect(url_for('home.index', id=to_login.id))
-        elif response.status_code == 200:
-            # user already exists
-            flash('User already exists!')
-            return render_template('create_user.html', form=form)
-        else:
-            flash('Unexpected response from users microservice!')
-            return render_template('create_user.html', form=form)
-    else:
-        for fieldName, errorMessages in form.errors.items():
-            for errorMessage in errorMessages:
-                flash('The field %s is incorrect: %s' % (fieldName, errorMessage))
-
-    return render_template('create_user.html', form=form)
+    if request.method == "GET":
+        return render_template("create_user.html", form=form)
+    elif request.method == "POST":
+        if form.is_submitted():
+            email = form.data["email"]
+            password = form.data["password"]
+            firstname = form.data["firstname"]
+            lastname = form.data["lastname"]
+            dateofbirth = form.data["dateofbirth"]
+            date = dateofbirth.strftime("%Y-%m-%d")
+            # check if date of birth is valid (in the past)
+            if dateofbirth is None or date > datetime.date.today():
+                return render_template(
+                    "create_user.html",
+                    form=form,
+                    error="Date of birth cannot be empty or in the future",
+                )
+            response = UserManager.create_user(
+                email, password, firstname, lastname, date
+            )
+            if response.status_code == 201:
+                return redirect(url_for("auth.login"))
+            elif response.status_code == 200:
+                # user already exists
+                return render_template(
+                    "create_user.html",
+                    form=form,
+                    error="A user with that email already exists",
+                )
 
 
-@users.route('/delete_user/<int:id>', methods=['GET', 'POST'])
-@login_required
-def delete_user(id):
-    """Deletes the data of the user from the database.
+# @users.route("/delete_user/<int:id>", methods=["GET", "POST"])
+# @login_required
+# def delete_user(id):
+#     """Deletes the data of the user from the database.
 
-    Args:
-        id_ (int): takes the unique id as a parameter
+#     Args:
+#         id_ (int): takes the unique id as a parameter
 
-    Returns:
-        Redirects the view to the home page
-    """
+#     Returns:
+#         Redirects the view to the home page
+#     """
 
-    response = UserManager.delete_user(id)
-    if response.status_code != 202:
-        flash("Error while deleting the user")
-        return redirect(url_for('auth.profile', id=id))
-        
-    return redirect(url_for('home.index'))
+#     response = UserManager.delete_user(id)
+#     if response.status_code != 202:
+#         flash("Error while deleting the user")
+#         return redirect(url_for("auth.profile", id=id))
 
-@users.route('/password', methods=['GET'])
-def change_pass_user_page():  # noqa: E501
+#     return redirect(url_for("home.index"))
+
+
+@users.route("/password", methods=["GET", "POST"])
+def change_pass_user():  # noqa: E501
     """Render change password template
      # noqa: E501
     :rtype: None
     """
-    form=ChangePassForm
-    return render_template('reset_password', form=form, error="")
+    form = ChangePassForm()
+    if request.method == "GET":
+        return render_template("reset_password.html", form = form)
+    elif request.method == "POST":
+        user_id = current_user.id 
+        currpw = form.currentpassword.data
+        newpw = form.newpassword.data
+        confpw = form.confirmationpassword.data
+        response = UserManager.change_password(user_id, currpw, newpw, confpw)
+        if response.status_code == 200:
+            return render_template("reset_password.html", form = form, success = "Password updated!")
+        elif response.status_code == 401:
+            return render_template("reset_password.html", form = form, error = "Wrong current password!")
+        elif response.status_code == 422:
+            return render_template("reset_password.html", form = form, error = "Check the correctness of the confirmation password!")
+        elif response.status_code == 404:
+            return abort(500)
 
 
-def change_pass_user(body=None):  # noqa: E501
-    """Change password for current user
-
+@users.route("/content_filter", methods=["GET", "POST"])
+def content_filter():  # noqa: E501
+    """Set content filter
      # noqa: E501
-
-    :param body: 
-    :type body: dict | bytes
-
     :rtype: None
     """
-   
-    if connexion.request.is_json:
-            body = UserPasswordBody.from_dict(connexion.request.get_json())
-   
-    return 'do some magic!'
-
-@users.route('/content_filter', methods=['GET'])
-def content_filter_page():  # noqa: E501
-    """Render content filter page
-     # noqa: E501
-    :rtype: None
-    """
-    return render_template("content_filter.html", feedback="")
+    if request.method == "GET":
+        return render_template("content_filter.html")
+    elif request.method == "POST":
+        user_id = current_user.id
+        filter = request.form["filter"]
+        response = UserManager.set_content_filter(user_id, filter)
+        if response.status_code == 200:
+            string = "enabled" if filter == 1 else "disabled"
+            return render_template("content_filter.html", feedback = "Your content filter has been "+string)
+        elif response.status_code == 404:
+            return abort(500)
 
 
-def set_content_filter(body=None):  # noqa: E501
-    """Change the content filter state
-
-     # noqa: E501
-
-    :param body: 
-    :type body: dict | bytes
-
-    :rtype: None
-    """
-    if connexion.request.is_json:
-        body = UserContentFilterBody.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
-
-
-def create_user_page():  # noqa: E501
-    """Render sign up template
-
-     # noqa: E501
-
-
-    :rtype: None
-    """
-    return 'do some magic!'
-
-@users.route('/blacklist', methods=['GET'])
+@users.route("/blacklist", methods=["GET"])
 def get_black_list():  # noqa: E501
     """Render blacklist template
     Return the user's black list page # noqa: E501
     :rtype: None
     """
-    current_user=UserManager.get_user_by_id(User.id)
+    current_user = UserManager.get_user_by_id(User.id)
     f = BlackListForm()
 
-    #TODO: get black_list
-    black_list=None
+    # TODO: get black_list
+    black_list = None
 
     return render_template(
         "black_list.html", form=f, black_list=black_list, size=len(black_list)
     )
+
 
 def update_black_list(body):  # noqa: E501
     """Update the black list for a user
 
     Update user black list adding or removing an user # noqa: E501
 
-    :param body: 
+    :param body:
     :type body: dict | bytes
 
     :rtype: InlineResponse200
     """
     if connexion.request.is_json:
         body = UserBlacklistBody.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    return "do some magic!"
 
 
-@users.route('/report')
+@users.route("/report", methods=["GET", "POST"])
 def report_page():  # noqa: E501
     """Render report page
      # noqa: E501
     :rtype: None
     """
+    #if request.method == "GET":
+        
+    # TODO: gestire errori
+    return render_template("report_user.html")
 
-    #TODO: gestire errori
-    return render_template('report_user.html')
 
-
-def report(body=None):  # noqa: E501
-    """Report a user
-
-     # noqa: E501
-
-    :param body: 
-    :type body: dict | bytes
-
-    :rtype: None
-    """
-    if connexion.request.is_json:
-        body = UserReportBody.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
-
-@users.route('/search_bar')
+@users.route("/search_bar")
 def search_user():  # noqa: E501
     """Render the search user page
 
@@ -208,19 +178,21 @@ def unregister():  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    return "do some magic!"
 
-@users.route('/user', methods=['GET'])
+
+@users.route("/user", methods=["GET"])
 def user_profile():  # noqa: E501
     """Render profile template of current user
      # noqa: E501
     :rtype: None
     """
     if User.is_authenticated():
-        current_user=UserManager.get_user_by_id(User.id)
-        return render_template('customer_profile.html',user=current_user)
+        current_user = UserManager.get_user_by_id(User.id)
+        return render_template("customer_profile.html", user=current_user)
     else:
-        return redirect(url_for('home.index',code=302))
+        return redirect(url_for("home.index", code=302))
+
 
 def users_list_json():  # noqa: E501
     """Return users list
@@ -230,28 +202,29 @@ def users_list_json():  # noqa: E501
 
     :rtype: List[InlineResponse2001]
     """
-    return 'do some magic!'
+    return "do some magic!"
 
-@users.route('/user', methods=['POST'])
+
+@users.route("/user", methods=["POST"])
 def update_user(body):  # noqa: E501
     """Updates the fields for the current user
 
      # noqa: E501
 
-    :param body: 
+    :param body:
     :type body: dict | bytes
 
     :rtype: None
     """
     if User.is_authenticated():
-        form=UserForm()
+        form = UserForm()
 
         if (
             request.form["Firstname"] == ""
             or request.form["Lastname"] == ""
             or request.form["Birthday"] == ""
             or request.form["Password"] == ""
-            or request.form["Email"]== ""
+            or request.form["Email"] == ""
         ):
             return render_template(
                 "update_customer.html",
@@ -259,28 +232,27 @@ def update_user(body):  # noqa: E501
                 user=User.__getattribute__,
                 error="All fields must be completed or the email is already associated to an account",
             )
-        json_obj={
+        json_obj = {
             "email": request.form["Email"],
-            "firstname":request.form["Firstname"],
-            "lastname":request.form["Lastname"],
-            "dateofbirth":request.form["Birthday"]
+            "firstname": request.form["Firstname"],
+            "lastname": request.form["Lastname"],
+            "dateofbirth": request.form["Birthday"],
         }
-        
-        #TODO: chiamare microservizio per modificare valori passando quel json
+
+        # TODO: chiamare microservizio per modificare valori passando quel json
     else:
-        return redirect(url_for('home.index',code=302))
+        return redirect(url_for("home.index", code=302))
 
     return
 
-@users.route('/users', methods= ['GET'])
+
+@users.route("/users", methods=["GET"])
 def users_list():  # noqa: E501
     """Render users list template
      # noqa: E501
     :rtype: None
     """
-    #TODO chiamata a microservizio
-    current_user=None
+    # TODO chiamata a microservizio
+    current_user = None
 
-    return render_template('users.html',user=current_user)
-
-
+    return render_template("users.html", user=current_user)
